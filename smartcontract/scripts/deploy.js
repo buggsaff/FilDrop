2d9d4797f6b3c138480eaa82bb8c016eeecbc8df
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-06-14 19:04:07 +0530

    add: smart contract using hardhat
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

---
+++ smartcontract/scripts/deploy.js
@@ -0,0 +1,30 @@
+require("@nomiclabs/hardhat-ethers");
+module.exports = {
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {
+    },
+    matic: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: ["0xab5694775b36b4cC86E5288f4a6A906C82C8EF1C"]
+    }
+  },
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
+}
